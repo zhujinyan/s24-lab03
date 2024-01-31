d392829f40bd1a9e7baba8c56b9ab6082d9218af
Jinyan Zhu <jinyanzhu@jinyans-mbp.wifi.local.cmu.edu>
2024-01-31 09:47:41 -0500

    Fix bugs in ArrayIntQueue

--- Java/src/main/java/edu/cmu/cs/cs214/rec02/ArrayIntQueue.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec02/ArrayIntQueue.java
@@ -73,11 +73,14 @@ public class ArrayIntQueue implements IntQueue {
 
     /** {@inheritDoc} */
     public boolean isEmpty() {
-        return size >= 0;
+        return size == 0;
     }
 
     /** {@inheritDoc} */
     public Integer peek() {
+        if (size == 0) {
+            return null;
+        }
         return elementData[head];
     }
 
